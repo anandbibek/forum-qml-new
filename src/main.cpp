1734784d50975d24f9e1b47bddf17896592acdfa
Philipp Zabel <philipp.zabel@gmail.com>
2011-11-24 21:27:15 +0100

    Set application name (needed to put the forum list cache in the correct place)

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,15 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     QScopedPointer<QApplication> app(createApplication(argc, argv));
     QScopedPointer<QmlApplicationViewer> viewer(QmlApplicationViewer::create());
 
+    QCoreApplication::setApplicationName(QFileInfo(QCoreApplication::applicationFilePath()).fileName());
+
     // Export the ForumSession and Thread classes
     qmlRegisterType<ForumSession>("Forum", 1, 0, "ForumSession");
     qmlRegisterUncreatableType<Thread>("Forum", 1, 0, "Thread", "Thread object creation is handled by the thread list model");
 
     viewer->setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
 
-    viewer->setMainQmlFile("qml/" + QFileInfo(QCoreApplication::applicationFilePath()).fileName() + "/main.qml");
+    viewer->setMainQmlFile("qml/" + QCoreApplication::applicationName() + "/main.qml");
     viewer->showExpanded();
 
     return app->exec();
